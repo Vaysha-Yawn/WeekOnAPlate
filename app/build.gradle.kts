6b932a151852fb016ad95a706cd4646bbd085307
Vaysha-Yawn <Viktoriyakokorina2001@gmail.com>
2025-06-16 19:25:14 +0700

    Fix privacy policy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "week.on.a.plate"
         minSdk = 28
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.3"
+        versionCode = 7
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
